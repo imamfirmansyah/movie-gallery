9f010022b04a20a4f0d2f412574f933568d88250
Imam Firmansyah <imamfirmansyah27@gmail.com>
2017-02-05 15:57:59 +0700

    Added Movie and Genre Movie Model

---
+++ src/MovieGallery/MovieGallery/Models/Movie.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MovieGallery.Models
+{
+    public class Movie
+    {
+        public int Id { get; set; }
+        public string MovieTitle { get; set; }
+        public string YearsRelease { get; set; }
+        public string StoryLineHtml { get; set; }
+        public Genre[] Genres { get; set; }
+    }
+}
