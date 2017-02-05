9b4389b34dbf95ad41a8da0132c69918f2ae7e0e
Imam Firmansyah <imamfirmansyah27@gmail.com>
2017-02-05 13:41:44 +0700

    Updated Movie Gallery Detail View

--- src/MovieGallery/MovieGallery/Controllers/MovieController.cs
+++ src/MovieGallery/MovieGallery/Controllers/MovieController.cs
@@ -10,6 +10,24 @@ namespace MovieGallery.Controllers
     {
         public ActionResult Detail()
         {
+            ViewBag.MovieTitle = "Big Hero 6";
+            ViewBag.YearsRelease = "2014";
+            ViewBag.StoryLine = "<p>When a devastating event befalls the city of San Fransokyo and catapults " +
+                            "Hiro into the midst of danger, he turns to Baymax and his close friends " +
+                            "adrenaline junkie Go Go Tomago, neatnik Wasabi, chemistry whiz Honey Lemon " +
+                            "and fanboy Fred. Determined to uncover the mystery, Hiro transforms his " +
+                            "friends into a band of high-tech heroes called &quot;Big Hero 6&quot;.</p>";
+            ViewBag.Genres = new string[]
+            {
+                "Animation",
+                "Action",
+                "Adventure",
+                "Comedy",
+                "Drama",
+                "Family",
+                "Sci-Fi"
+            };
+
             return View();
         }
     }
