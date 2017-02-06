745c3d3e9c10d935aea44c52e81fdc21cc44c6a3
Imam Firmansyah <imamfirmansyah27@gmail.com>
2017-02-06 11:47:35 +0700

    Updated Movie Gallery Detail view to be strongly typed

--- src/MovieGallery/MovieGallery/Controllers/MovieController.cs
+++ src/MovieGallery/MovieGallery/Controllers/MovieController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using MovieGallery.Models;
 
 namespace MovieGallery.Controllers
 {
@@ -10,25 +11,29 @@ namespace MovieGallery.Controllers
     {
         public ActionResult Detail()
         {
-            ViewBag.MovieTitle = "Big Hero 6";
-            ViewBag.YearsRelease = "2014";
-            ViewBag.StoryLine = "<p>When a devastating event befalls the city of San Fransokyo and catapults " +
+            var movieGallery = new Movie()
+            {
+                MovieTitle = "Big Hero 6",
+                YearsRelease = "2014",
+                StoryLineHtml = "<p>When a devastating event befalls the city of San Fransokyo and catapults " +
                             "Hiro into the midst of danger, he turns to Baymax and his close friends " +
                             "adrenaline junkie Go Go Tomago, neatnik Wasabi, chemistry whiz Honey Lemon " +
                             "and fanboy Fred. Determined to uncover the mystery, Hiro transforms his " +
-                            "friends into a band of high-tech heroes called &quot;Big Hero 6&quot;.</p>";
-            ViewBag.Genres = new string[]
-            {
-                "Animation",
-                "Action",
-                "Adventure",
-                "Comedy",
-                "Drama",
-                "Family",
-                "Sci-Fi"
+                            "friends into a band of high-tech heroes called &quot;Big Hero 6&quot;.</p>",
+                Genres = new Genre[]
+                {
+                    new Genre() { Name = "Animation" },
+                    new Genre() { Name = "Action" },
+                    new Genre() { Name = "Adventure" },
+                    new Genre() { Name = "Comedy" },
+                    new Genre() { Name = "Drama" },
+                    new Genre() { Name = "Family" },
+                    new Genre() { Name = "Sci-Fi" },
+                }
+
             };
 
-            return View();
+            return View(movieGallery);
         }
     }
 }
