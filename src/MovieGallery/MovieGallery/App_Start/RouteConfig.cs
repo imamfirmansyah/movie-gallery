14af1b9d10a06b792ba5f5cb347ac1f18d0bb2c2
Imam Firmansyah <imamfirmansyah27@gmail.com>
2017-02-10 21:51:20 +0700

    Updated Default Route

--- src/MovieGallery/MovieGallery/App_Start/RouteConfig.cs
+++ src/MovieGallery/MovieGallery/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace MovieGallery
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Movie", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
